bb81fce2a6bb0ba30d715b468daa2f59f0c791f7
Vidhya100 <vidhyadarade1@gmail.com>
2022-09-22 23:33:03 +0530

    Use of settter and getter

--- Pages/js/EmployeePayRoll.js
+++ Pages/js/EmployeePayRoll.js
@@ -18,4 +18,42 @@ class EmployeePayrollData{
     set startDate(startDate){
         this._startDate = startDate;
     }
+
+    get id() { return this._id; }
+    set id(id){
+        this._id = id;
+    }
+
+    get profilePic() { return this._profilePic; }
+    set profilePic(profilePic){
+        this._profilePic = profilePic;
+    }
+
+    get gender() { return this._gender; }
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get department() { return this._department; }
+    set department(department){
+        this._department = department;
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get note() { return this._note; }
+    set note(note){
+        this._note = note;
+    }
+
+    toString(){
+        const options = { year:'numeric' , month:'long', day:'numeric' };
+        const empDate = !this.startDate? "undefined" : this.startDate.toLocalDateString("en-US", options);
+
+        return "id="+this.id+", name="+ this.name + ", gender="+this.gender + ", profilePic="+this.profilePic+", department="+this.department + ", salary="+ this.salary +", startDate="+empDate + ", note="+this.note;
+
+    }
 }
