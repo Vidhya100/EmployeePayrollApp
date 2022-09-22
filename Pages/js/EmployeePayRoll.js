c8a9d19a7077ace51b0104bc74dfcf915f40db62
Vidhya100 <vidhyadarade1@gmail.com>
2022-09-22 23:15:35 +0530

    Use of set and get for Name And startDate

--- Pages/js/EmployeePayRoll.js
+++ Pages/js/EmployeePayRoll.js
@@ -4,3 +4,18 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+
+class EmployeePayrollData{
+    get name() { return this._name; }
+    set name(name){
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-z\\]{2,}$')
+        if(nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorrect';
+    }
+
+    get startDate() { return this._startDate; }
+    set startDate(startDate){
+        this._startDate = startDate;
+    }
+}
